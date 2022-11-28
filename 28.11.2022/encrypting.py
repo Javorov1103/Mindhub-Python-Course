f95e00743dde54ff692276d5c551d38c351f5aca
Kalin Javorov <k.javorov@gmail.com>
2022-11-28 20:05:47 +0200

    asd

--- 28.11.2022/encrypting.py
+++ 28.11.2022/encrypting.py
@@ -11,6 +11,7 @@ def get_message():
 def is_even(number):
     return number % 2 == 0
 
+# Get the letters of the message, which are on the even indexes
 def get_even_letters(message):
     even_letters = []
     for i in range(0, len(message)):
@@ -18,6 +19,7 @@ def get_even_letters(message):
             even_letters.append(message[i])
     return even_letters
 
+# Get the letters of the message, which are on the odd indexes
 def get_odd_letters(message):
     odd_letters = []
     for i in range(0,len(message)):
@@ -25,6 +27,19 @@ def get_odd_letters(message):
             odd_letters.append(message[i])
     return odd_letters
 
+def swap_letters(message):
+    letter_list = []
+    if not is_even(len(message)):
+        message = message +'x'
+    even_letters = get_even_letters(message)
+    odd_letters = get_odd_letters(message)
+
+    for i in range(0, int(len(message)/2)):
+        letter_list.append(odd_letters[i])
+        letter_list.append(even_letters[i])
+    new_message = ''.join(letter_list)
+    return new_message
+    
 root = Tk()
 
 while True:
