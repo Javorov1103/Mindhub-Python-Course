09afbb45b8a7d3830897f894607cd03aab1dccf9
Kalin Javorov <k.javorov@gmail.com>
2022-12-12 20:51:24 +0200

    Exer

---
+++ 12.12.2022/message_system.py
@@ -0,0 +1,26 @@
+number = int(input())  
+def read_and_process_message():
+    message_type = input()
+    match message_type:
+        case 'success':
+            operation = input()
+            message = input()
+            show_success_message(operation,message)
+        case 'warning':
+            message = input()
+            show_warning_message(message)
+        case 'error':
+            operation = input()
+            message = input()
+            error_code = input()
+            show_error_message(operation,message,error_code)
+            
+def show_success_message(operation,message):
+    print()
+def show_warning_message(message):
+    print()
+def show_error_message(operation,message,error_code):
+    print()
+    
+for i in range(number):
+    read_and_process_message()
