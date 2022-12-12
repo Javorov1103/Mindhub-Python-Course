4af176c24603d68b819f009264d2187aa4c36816
Kalin Javorov <k.javorov@gmail.com>
2022-12-12 19:58:08 +0200

    next shape

--- 12.12.2022/snap.py
+++ 12.12.2022/snap.py
@@ -38,5 +38,35 @@ c.pack()
 
 random.shuffle(shapes)
 
+shape = None
+previous_color = ''
+current_color = ''
+player1_score = 0
+player2_score = 0
+
+def next_shape():
+    global shape
+    global previous_color
+    global current_color
+    
+    previous_color = current_color
+
+    c.delete(shape)
+    if len(shapes) > 0:
+        shape = shapes.pop()
+        c.itemconfig(shape, state= NORMAL)
+        current_color = c.itemcget(shape, 'fill')
+        root.after(1000, next_shape)
+    else:
+        c.unbind('q')
+        c.unbind('p')
+        if player1_score > player2_score:
+            c.create_text(200,200, text="Winner Player 1")
+        elif player2_score > player1_score:
+            c.create_text(200,200, text="Winner Player 2")
+        else:
+            c.create_text(200,200, text='Draw')
+        c.pack()
+        
 
 
