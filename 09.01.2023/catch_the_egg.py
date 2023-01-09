4a4ee4d85cebd5da290d08ba64c6a4f5de465b47
Kalin Javorov <k.javorov@gmail.com>
2023-01-09 19:25:36 +0200

    Update catch_the_egg.py

--- 09.01.2023/catch_the_egg.py
+++ 09.01.2023/catch_the_egg.py
@@ -3,7 +3,8 @@ from random import randrange
 from tkinter import Canvas, Tk,messagebox,font
 
 canvas_width = 800
-convas_height = 400
+canvas_height = 400
 
 root = Tk()
-c = Canvas(root, width=canvas_width,height=convas_height, background='deep sky blue')
+c = Canvas(root, width=canvas_width,height=canvas_height, background='deep sky blue')
+c.create_rectangle(-5, canvas_height-100, canvas_width+5,canvas_height+5, fill="sea green", width=0)
