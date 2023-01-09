904c4937ab89e2cbd09be733b7cce3a0b3a0b331
Kalin Javorov <k.javorov@gmail.com>
2023-01-09 19:27:44 +0200

    asd

--- 09.01.2023/catch_the_egg.py
+++ 09.01.2023/catch_the_egg.py
@@ -8,3 +8,5 @@ canvas_height = 400
 root = Tk()
 c = Canvas(root, width=canvas_width,height=canvas_height, background='deep sky blue')
 c.create_rectangle(-5, canvas_height-100, canvas_width+5,canvas_height+5, fill="sea green", width=0)
+c.create_oval(-80,-80, 120,120,fill='orange', width=0)
+c.pack()
